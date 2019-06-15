5471ff16b86d3986a0c1a97454733113354369fc
MGSA <mgsalton@gmail.com>
2019-06-15 18:43:44 +0100

    Successfully implemented: - character and enemy now renders on screen

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+/*var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -20,28 +20,55 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+*/
+
+function EnemyGeneration() {
+    this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 50;
+
+    this.update = function(dt) {
+
+    };
+
+    this.render = function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-let player = {
-    update: function(){
+function PlayerGeneration(name) {
 
-    },
-    render: function(){
+    this.name = name;
+    this.sprite = 'images/char-boy.png';
+    this.x = 300;
+    this.y = 400;
 
-    },
-    handleInput: function(){
+    this.update = function(){
 
-    }    
+    };
+
+    this.render = function(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+
+    this.handleInput = function(){
+
+    };
+
 };
+
+let player = new PlayerGeneration('Mathew');
+let enemy = new EnemyGeneration();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-let allEnemies = [];
+let allEnemies = [enemy];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
